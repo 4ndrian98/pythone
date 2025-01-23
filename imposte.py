49b9a0831541e8300d8ef87a659461b0983819ac
Andrian Ghiba <andrian.ghiba98@gmail.com>
2025-01-23 13:13:17 +0100

    Update imposte.py

--- imposte.py
+++ imposte.py
@@ -1,35 +1,32 @@
-def calcolo_tasse(reddito):
-    PRIMA_ALIQUOTA = 0.23
-    SECONDA_ALIQUOTA = 0.35
-    TERZA_ALIQUOTA = 0.43
 
-    SCAGLIONE_1 = 28000
-    SCAGLIONE_2 = 50000
+PRIMA_ALIQUOTA = 0.23
+SECONDA_ALIQUOTA = 0.35
+TERZA_ALIQUOTA = 0.43
 
-    reddito = float(input("Inserisci il tuo reddito: "))
-    tasse_da_scontare = 0.0
-    reddito_netto = 0.0
+SCAGLIONE_1 = 28000
+SCAGLIONE_2 = 50000
 
-    if reddito > SCAGLIONE_1:
-        tasse_da_scontare += SCAGLIONE_1 * PRIMA_ALIQUOTA
+reddito = float(input("Inserisci il tuo reddito: "))
+tasse_da_scontare = 0.0
+reddito_netto = 0.0
+
+if reddito > SCAGLIONE_1:
+    tasse_da_scontare += SCAGLIONE_1 * PRIMA_ALIQUOTA
+else:
+    tasse_da_scontare += reddito * PRIMA_ALIQUOTA
+
+if reddito > SCAGLIONE_1:
+    if reddito <= SCAGLIONE_2:
+        tasse_da_scontare += (reddito - SCAGLIONE_1) * SECONDA_ALIQUOTA
     else:
-        tasse_da_scontare += reddito * PRIMA_ALIQUOTA
+        tasse_da_scontare += (SCAGLIONE_2 - SCAGLIONE_1) * SECONDA_ALIQUOTA
 
-    if reddito > SCAGLIONE_1:
-        if reddito <= SCAGLIONE_2:
-            tasse_da_scontare += (reddito - SCAGLIONE_1) * SECONDA_ALIQUOTA
-        else:
-            tasse_da_scontare += (SCAGLIONE_2 - SCAGLIONE_1) * SECONDA_ALIQUOTA
+if reddito > SCAGLIONE_2:
+    tasse_da_scontare += (reddito - SCAGLIONE_2) * TERZA_ALIQUOTA
 
-    if reddito > SCAGLIONE_2:
-        tasse_da_scontare += (reddito - SCAGLIONE_2) * TERZA_ALIQUOTA
+print("Le tasse da pagare sono:", tasse_da_scontare, "€")
 
-    print("Le tasse da pagare sono:", tasse_da_scontare, "€")
+reddito_netto = reddito - tasse_da_scontare
+print("Il tuo reddito al netto delle tasse è:", reddito_netto, "€")
+return reddito_netto
 
-    reddito_netto = reddito - tasse_da_scontare
-    print("Il tuo reddito al netto delle tasse è:", reddito_netto, "€")
-    return reddito_netto
-
-
-tasse=calcolo_tasse(53000)
-print(tasse)
