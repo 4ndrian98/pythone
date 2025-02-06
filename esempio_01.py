422129b65799ca09fb52d8e911d55817d917bf1a
Andrian Ghiba <andrian.ghiba98@gmail.com>
2025-02-06 09:22:04 +0100

    Add files via upload

---
+++ esempio_01.py
@@ -0,0 +1,7 @@
+
+variabile1= 5.0
+while True:
+    variabile2=float(input("Scrivi un numero. Per fermare il ciclo per due volte scrivi 0: "))
+    if variabile1 == 0 and variabile1 == variabile2:
+        break
+    else: variabile1=variabile2
