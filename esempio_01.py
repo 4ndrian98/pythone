3a16717de1754537278f9b4826bb649f1e1ba5d1
Andrian Ghiba <andrian.ghiba98@gmail.com>
2025-02-06 09:23:50 +0100

    Update esempio_01.py

--- esempio_01.py
+++ esempio_01.py
@@ -1,7 +1,9 @@
-
 variabile1= 5.0
+somma=0.0
 while True:
     variabile2=float(input("Scrivi un numero. Per fermare il ciclo per due volte scrivi 0: "))
+    somma+=variabile2
     if variabile1 == 0 and variabile1 == variabile2:
         break
     else: variabile1=variabile2
+print("Ecco la tua somma:", somma)
