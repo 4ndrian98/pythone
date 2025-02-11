143f9949f59b9d384c3e0882e6a8560cf5450eac
Andrian Ghiba <andrian.ghiba98@gmail.com>
2025-02-11 09:53:21 +0100

    Update esempio_03.py

--- esempio_03.py
+++ esempio_03.py
@@ -1,12 +1,12 @@
 Punteggio_A=0
 Punteggio_B=0
 scambi=0
-ciclo=True
+game=True
 vincitore=0
-scambio_passato=0
+vincitore_scambio_passato=0
 x=0
 x2=0
-while ciclo:
+while game:
     vincitore=int(input("Chi ha vinto lo scambio? 1(per giocatore 1) o 2 (per giocatore 2): "))
     scambi+=1
     if vincitore == 1 and Punteggio_A <40:
@@ -20,14 +20,15 @@ while ciclo:
     if Punteggio_B >40:
         Punteggio_B = 40
         x2= scambi 
-    print("Scambio vinto dal Giocatore",vincitore,"Ecco il punteggio: Giocatore1",Punteggio_A,"-",Punteggio_B,"Giocatore2", x, x2)
-    if (scambi > x+2 and Punteggio_A == 40 and vincitore == 1 and vincitore == scambio_passato) or ( scambi > x+2 and Punteggio_B == 40 and vincitore == 2 and vincitore == scambio_passato):
-        ciclo=False
-    scambio_passato=vincitore
+    print("Scambio",scambi, "vinto dal Giocatore",vincitore,"Ecco il punteggio: Giocatore1",Punteggio_A,"-",Punteggio_B,"Giocatore2",x, x2)
+    if (scambi > x+1 and Punteggio_A == 40 and vincitore == 1 and vincitore == vincitore_scambio_passato) or ( scambi > x+1 and Punteggio_B == 40 and vincitore == 2 and vincitore == vincitore_scambio_passato):
+        game=False
+    vincitore_scambio_passato= vincitore
     
 
 print("Ha vinto il giocatore", vincitore)
                   
+                
                 
                   
                 
